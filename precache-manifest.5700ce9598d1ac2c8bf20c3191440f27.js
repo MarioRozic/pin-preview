68d4e554387e4fe782606585b98da1489cdb3bcc
MarioRozic <rozic.mario@hotmail.com>
2020-08-29 22:54:47 +0200

    Updates

---
+++ precache-manifest.5700ce9598d1ac2c8bf20c3191440f27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89506cf818528a569099ba140d75acd3",
+    "url": "/pin-preview/index.html"
+  },
+  {
+    "revision": "4c2b80f31e118aef1010",
+    "url": "/pin-preview/static/css/main.13736888.chunk.css"
+  },
+  {
+    "revision": "a35866f570d74379d670",
+    "url": "/pin-preview/static/js/2.3c707bf1.chunk.js"
+  },
+  {
+    "revision": "2fe91ab6e630ee13cf2df1e9d9c2095b",
+    "url": "/pin-preview/static/js/2.3c707bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c2b80f31e118aef1010",
+    "url": "/pin-preview/static/js/main.05808f72.chunk.js"
+  },
+  {
+    "revision": "8c4fbd9f573c898f17b9",
+    "url": "/pin-preview/static/js/runtime-main.6572c554.js"
+  }
+]);
